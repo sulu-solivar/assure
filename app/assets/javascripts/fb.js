5c287cc2629fd6a11ba77fd3232b60cac3709fb0
Eshwar Chandra <eshwarcc@gmail.com>
2012-06-26 01:00:03 +0530

    Corrected fb.js bug

--- app/assets/javascripts/fb.js
+++ app/assets/javascripts/fb.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
   window.APP_KEY = $('body').attr('app_key')
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      
+      appId      : window.APP_KEY, // App ID
+      // channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional initialization code here
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
 })
-
-window.fbAsyncInit = function() {
-  FB.init({
-    
-    appId      : window.APP_KEY, // App ID
-    // channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
-
-  // Additional initialization code here
-};
-
-// Load the SDK Asynchronously
-(function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
- }(document));
 
 function fb_login ()
 {
